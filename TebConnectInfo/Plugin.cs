64f9dc57b5b44aefcc7fe81f401463c2dd41197b
tebbeh <emil@igypt.se>
2023-11-10 21:39:26 +0100

    Create new mod

---
+++ TebConnectInfo/Plugin.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using System.Net;
+
+namespace Tebbeh.TebConnectInfo
+{
+    [BepInDependency(Jotunn.Main.ModGuid)]
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class TebConnectInfo : BaseUnityPlugin
+    {
+        private const string ModName = "TebConnectInfo";
+        private const string ModVersion = "0.1.0";
+        private const string Author = "com.tebbeh.mod";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = BepInEx.Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+
+        private readonly Harmony HarmonyInstance = new(ModGUID);
+
+        public static readonly ManualLogSource TebLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        #region ConfigurationEntries
+
+        internal static ConfigEntry<string> WebhookURL = null!;
+        internal static ConfigEntry<string> WebhookName = null!;
+        internal static ConfigEntry<string> joinMessage = null!;
+        internal static ConfigEntry<string> leaveMessage = null!;
+
+        public static string GetWebhookURL() => WebhookURL.Value;
+        public static string GetWebhookName() => WebhookName.Value;
+        public static string GetjoinMessage() => joinMessage.Value;
+        public static string GetleaveMessage() => leaveMessage.Value;
+
+        private readonly ConfigurationManagerAttributes AdminConfig = new ConfigurationManagerAttributes { IsAdminOnly = true };
+        private readonly ConfigurationManagerAttributes ClientConfig = new ConfigurationManagerAttributes { IsAdminOnly = false };
+
+        private void AddConfig<T>(string key, string section, string description, bool synced, T value, ref ConfigEntry<T> configEntry)
+        {
+            string extendedDescription = GetExtendedDescription(description, synced);
+            configEntry = Config.Bind(section, key, value,
+                new ConfigDescription(extendedDescription, null, synced ? AdminConfig : ClientConfig));
+        }
+        public string GetExtendedDescription(string description, bool synchronizedSetting)
+        {
+            return description + (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]");
+        }
+
+        #endregion
+
+        void Awake()
+        {
+            #region Configuration
+
+            const string general = "General";
+            const string messages = "Messages";
+
+            AddConfig("WebhookURL", general, "Webhook url (string).", true, "", ref WebhookURL);
+            AddConfig("WebhookName", general, "Webhook send as username (string).", true, "", ref WebhookName);
+            AddConfig("JoinMessage", messages, "Join message to post with webhook, manipulate after your needs. Standard is for Discord! (string).", true, ":green_circle: **{player}** has come online!", ref joinMessage);
+            AddConfig("LeaveMessage", messages, "Leave message to post with webhook, manipulate after your needs. Standard is for Discord! (string).", true, ":red_circle: **{player}** gone offline!", ref leaveMessage);
+
+            #endregion
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            HarmonyInstance.PatchAll(assembly);
+
+            SetupWatcher();
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(BepInEx.Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                TebLogger.LogDebug("Attempting to reload configuration...");
+                Config.Reload();
+            }
+            catch
+            {
+                TebLogger.LogError($"There was an issue loading {ConfigFileName}");
+            }
+        }
+
+
+        private static void PostToDiscord(string content)
+        {
+            string webhookURL = WebhookURL.Value;
+            string webhookUsername = WebhookName.Value;
+
+            if (content == "" || webhookURL == "")
+            {
+                return;
+            }
+
+            WebRequest discordAPI = WebRequest.Create(webhookURL);
+            discordAPI.Method = "POST";
+            discordAPI.ContentType = "application/json";
+
+            discordAPI.GetRequestStreamAsync().ContinueWith(t =>
+            {
+                static string escape(string s) => s.Replace("\\", "\\\\").Replace("\"", "\\\"");
+
+                using StreamWriter writer = new(t.Result);
+                string json = @"{""content"":""" + escape(content) + @"""" + (webhookUsername == "" ? "" : @", ""username"":""" + escape(webhookUsername) + @"""") + "}";
+                writer.WriteAsync(json).ContinueWith(_ => discordAPI.GetResponseAsync());
+            });
+        }
+
+        internal class GetConnectInfo
+        {
+
+            [HarmonyPatch(typeof(Chat), nameof(Chat.OnNewChatMessage))]
+            private class OnConnect
+            {
+                private static void Prefix(ref UserInfo user)
+                {
+                    string playername = user.Name;
+                    if (string.IsNullOrEmpty(playername))
+                    {
+                        return;
+                    }
+
+                    string postdata = TebConnectInfo.GetjoinMessage();
+                    postdata = postdata.Replace("{player}", playername);
+
+                    PostToDiscord(postdata);
+                    
+                }
+            }
+
+            [HarmonyPatch(typeof(ZNet), nameof(ZNet.RPC_Disconnect))]
+            internal class RPC_Disconnect
+            {
+                private static void Prefix(ZRpc rpc)
+                {
+                    ZNetPeer peer = ZNet.instance.GetPeer(rpc);
+
+                    if (peer == null) { return; }
+
+                    string playername = peer.m_playerName;
+
+                    if (playername.Length > 0)
+                    {
+                        string postdata = TebConnectInfo.GetleaveMessage();
+                        postdata = postdata.Replace("{player}", playername);
+
+                        PostToDiscord(postdata);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TebConnectInfo/README.md
@@ -0,0 +1,33 @@
+# TebBoatTweaks
+- Created by tebbeh 
+- Discord: tebbeh#0933
+
+# Introduction
+Send information by webhook when a player join or disconnect from a server.
+
+# Installation
+This mod is made for Server Side purpose but works for Client aswell.
+1. Put .dll file in Valheim/BepInEx/plugins/TebConnectInfo
+2. Start Server/Client once to generate Config
+3. Change values in config for your needs
+
+Extra: Create Webhook for Discord to post join and leave messages in a channel
+1. Make sure you have admin rights for the discord server or integration permissions
+2. Go to Server Settings
+3. Go to the category Apps -> Integrations
+4. Click the blue button "New Webhook"
+5. Give your webhook a name and point it to a channel
+6. Click "Copy Webhook URL"
+7. Paste it in the config for WebhookURL
+
+
+# Requirements
+- BepInEx - https://valheim.thunderstore.io/package/denikson/BepInExPack_Valheim/
+- Jotunn - https://valheim.thunderstore.io/package/ValheimModding/Jotunn/
+
+# Changelog
+- 0.1.0 | First release
+
+# Contributing
+- All issues can be reported on the project Github. To report issues please be as specific as possible.
+- All feedback, ideas, and requests are welcome! You can find me in most Valheim Mod Discords under the nickname "tebbeh".
